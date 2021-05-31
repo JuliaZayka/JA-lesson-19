28e2ea66484564b332c435f9042f613030b1aa0c
JuliaZayka <julia.zayka18@gmail.com>
2021-05-31 14:30:16 +0300

    lesson-19

---
+++ src/main/java/ua/lviv/service/StudentService.java
@@ -0,0 +1,34 @@
+package ua.lviv.service;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+import ua.lviv.domain.Student;
+import ua.lviv.repository.StudentRepository;
+
+@Service
+public class StudentService {
+
+	@Autowired
+	private StudentRepository studentRepository;
+
+	public Student saveStudent(String firstName, String lastName, Integer age, MultipartFile file) throws IOException {
+		String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+		Student student = null;
+
+		if (!fileName.contains("..")) {
+			student = new Student(firstName, lastName, age, fileName, file.getContentType(), file.getBytes());
+		}
+
+		return studentRepository.save(student);
+	}
+
+	public Student getStudentById(String id) throws FileNotFoundException {
+		return studentRepository.findById(id)
+				.orElseThrow(() -> new FileNotFoundException("File not fount with id = " + id));
+	}
+}
---
+++ src/test/java/ua/lviv/lesson19_fromLogos/Lesson19FromLogosApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.lviv.lesson19_fromLogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson19FromLogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
